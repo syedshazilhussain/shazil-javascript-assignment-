57de1302dfedae72cb9de36b312169b2e66aeeb0
syedshazilhussain <119860685+syedshazilhussain@users.noreply.github.com>
2022-12-05 17:04:35 +0500

    shazil javascript assignment
    shazil javascript assignment

---
+++ app.js
@@ -0,0 +1,28 @@
+var lider = [
+    "image/flowers.jpg",
+    "image/istockphoto.jpg",
+    "image/federa.jpg",
+    "image/avemaria.webp"
+]
+var oneImage = 0;
+var twoImage = lider.length -1;
+var lastImage = 0;
+
+var nextbtn = document.getElementById("nextReturn");
+function next() {
+    image = document.getElementById("image1");
+    lastImage++;
+    if(lastImage >= twoImage){
+        lastImage = 3;
+    }
+    image.src = lider[lastImage]
+}
+var previousbtn = document.getElementById("previousBack");
+function previous() {
+    image = document.getElementById("image1");
+    lastImage--;
+    if(lastImage <= oneImage){
+        lastImage = 0;
+    }
+    image.src = lider[lastImage]
+}
